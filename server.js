360ec458842d5498aa49a3b10c671dc2a195ce1f
i516435 <rosemol.wilson@sap.com>
2020-10-01 02:45:34 +0530

    Adding a post messages service

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var bodyParser = require("body-parser");
 
-app.use(express.static(__dirname))
+var app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var messages = [
-    {
-        name: 'Lilly',
-        message:'Hi'
-    },
-    {
-        name: 'Rose',
-        message:'Hello'
-    }
-]
+  {
+    name: "Lilly",
+    message: "Hi"
+  },
+  {
+    name: "Rose",
+    message: "Hello"
+  }
+];
 
-app.get('/messages', (req, res)=> {
-    res.send(messages)
-})
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
 
-var server = app.listen(3000, ()=> {
-    console.log('Server is listening on port', server.address().port)
-})
+app.post("/messages", (req, res) => {
+  messages.push(req.body);
+  res.sendStatus(200);
+});
 
+var server = app.listen(3000, () => {
+  console.log("Server is listening on port", server.address().port);
+});
