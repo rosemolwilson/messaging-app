10e1d0e25024fd7748f9f1716c47e79582111421
i516435 <rosemol.wilson@sap.com>
2020-10-01 12:44:43 +0530

    Saving messages to mongoDb

--- server.js
+++ server.js
@@ -1,49 +1,58 @@
-require('dotenv').config();
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
+require("dotenv").config()
+var express = require("express")
+var bodyParser = require("body-parser")
+var app = express()
 
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
-var mongoose = require('mongoose')
+var http = require("http").Server(app)
+var io = require("socket.io")(http)
+var mongoose = require("mongoose")
 
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-user=process.env.user
-password=process.env.password
-var dbUrl = 'mongodb+srv://'+user+':'+password+'@cluster0.b7xsa.mongodb.net/<dbname>?retryWrites=true&w=majority'
+user = process.env.user
+password = process.env.password
+var dbUrl =
+  "mongodb+srv://" +
+  user +
+  ":" +
+  password +
+  "@cluster0.b7xsa.mongodb.net/messages?retryWrites=true&w=majority"
 
-var messages = [
-  {
-    name: "Lilly",
-    message: "Hi"
-  },
-  {
-    name: "Rose",
-    message: "Hello"
-  }
-];
+var Message = mongoose.model("Message", {
+  name: String,
+  message: String
+})
 
 app.get("/messages", (req, res) => {
-  res.send(messages);
-});
+  Message.find({}, (err, messages) => {
+    res.send(messages)
+  })
+})
 
 app.post("/messages", (req, res) => {
-  messages.push(req.body);
-  io.emit('message', req.body)
-  res.sendStatus(200);
+  var message = new Message(req.body)
+
+  message.save(err => {
+    if (err) sendStatus(500)
+    io.emit("message", req.body)
+    res.sendStatus(200)
+  })
 })
 
-io.on('connection', (socket) => {
-    console.log("User connected.......")
+io.on("connection", socket => {
+  console.log("User connected.......")
 })
 
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err) =>{
-  console.log('Mongodb connection', err)
-})
+mongoose.connect(
+  dbUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    console.log("Mongodb connection", err)
+  }
+)
 
 var server = http.listen(3000, () => {
-  console.log("Server is listening on port", server.address().port);
-});
+  console.log("Server is listening on port", server.address().port)
+})
