9c8f9def8da2e02dcc37052986f9ffc9fed3f807
i516435 <rosemol.wilson@sap.com>
2020-10-01 12:29:20 +0530

    Connecting to MongoDB instance

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
+require('dotenv').config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+user=process.env.user
+password=process.env.password
+var dbUrl = 'mongodb+srv://'+user+':'+password+'@cluster0.b7xsa.mongodb.net/<dbname>?retryWrites=true&w=majority'
 
 var messages = [
   {
@@ -30,8 +36,12 @@ app.post("/messages", (req, res) => {
   res.sendStatus(200);
 })
 
-io.on('connection', (socket) =>{
-    console.log("User connected")
+io.on('connection', (socket) => {
+    console.log("User connected.......")
+})
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err) =>{
+  console.log('Mongodb connection', err)
 })
 
 var server = http.listen(3000, () => {
